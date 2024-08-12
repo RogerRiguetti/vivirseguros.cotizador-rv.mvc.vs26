d7af6fbc84c3b1e724ca65b7dc2d30cbb428d2c2
wcdzvs <william.chavez@vivirseguros.pe>
2024-08-12 17:32:41 -0500

    ultimos cambios

--- Estudio.Controllers/Controllers/JubilareController.cs
+++ Estudio.Controllers/Controllers/JubilareController.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+/// <summary>
+/// Controlador para acciones de Jubilare
+/// </summary>
+/// 
+/// <remarks>
+/// Este controlador proporciona acciones para exportar datos en diferentes formatos.
+///
+/// Creado por: @wcdz
+/// Fecha de creación: 2024-08-12
+/// </remarks>
 namespace Estudio.Controllers.Controllers
 {
     public class JubilareController : Controller
@@ -13,5 +23,34 @@ namespace Estudio.Controllers.Controllers
         {
             return View();
         }
+
+        public ActionResult ExportData()
+        {
+            // Nomencatura del nombre del archivo
+            // CarteraJubilare_2024_08_12_15_48.xlsx
+            string fileName = "CarteraJubilare";
+            DateTime now = DateTime.Now;
+
+            string formattedDate = now.ToString("yyyy_MM_dd");
+            string formattedTime = now.ToString("HH_mm_ss");
+            string fileNameWithDateTime = $"{fileName}_{formattedDate}_{formattedTime}.xlsx";
+
+            string getDataStoredProcedure = GetDataStoredProcedure("usp_Jub_Sel_CarteraCompleta");
+
+            Console.WriteLine(fileNameWithDateTime);
+
+            return Json(1, JsonRequestBehavior.AllowGet);
+        }
+
+        protected string GetDataStoredProcedure(string nameStoredProcedure)
+        {
+            string script = $"EXECUTE {nameStoredProcedure}";
+            // Determinar el ejecutable para conseguir la data
+            //VCEDBContext<DataTable>.CallSelectStatementDt(script, x => new DataTable());
+            return script;            
+        }
+
+
+
     }
 }
