df77f76faf5d3260bac2d3c3da9beacc4674e107
wcdzvs <william.chavez@vivirseguros.pe>
2024-08-26 16:50:44 -0500

    delete directivas demas

--- Estudio.Controllers/Controllers/JubilareController.cs
+++ Estudio.Controllers/Controllers/JubilareController.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using Estudio.Repository.Helpers;
-using System.Data;
-using System.Data.SqlClient;
-using Estudio.Repository;
-using Newtonsoft.Json;
 using System.IO;
 using log4net;
 using System.Reflection;
