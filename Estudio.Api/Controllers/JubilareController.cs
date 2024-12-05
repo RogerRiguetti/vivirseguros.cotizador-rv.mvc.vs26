b964fe86438c3c3b3368a2b7148b93fea6a2766e
wcdzvs <william.chavez@vivirseguros.pe>
2024-12-05 09:42:25 -0500

    ajustes para planilla

--- Estudio.Api/Controllers/JubilareController.cs
+++ Estudio.Api/Controllers/JubilareController.cs
@@ -87,10 +87,24 @@ namespace Estudio.Api.Controllers
 
             JubilareExportData jubilareExportData = new JubilareExportData();
 
-            var aux = jubilareExportData.ExportToExcel(filePath, id); // Generacion de la planilla
+            jubilareExportData.ExportToExcel(filePath, id); // Generacion de la planilla
 
+            // Retornar el archivo como descarga
+            return ResponseMessage(new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new ByteArrayContent(File.ReadAllBytes(filePath))
+                {
+                    Headers =
+                {
+                    ContentDisposition = new ContentDispositionHeaderValue("attachment")
+                    {
+                        FileName = fileName
+                    },
+                    ContentType = new MediaTypeHeaderValue("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                }
+                }
+            });
 
-            return Ok(aux);
             //return Ok(planilla);
 
             //return Ok($"{aux}");
