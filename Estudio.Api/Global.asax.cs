fbdcba4cba36ff8510275b8d83ee46565f951634
wcdzvs <william.chavez@vivirseguros.pe>
2024-12-18 10:52:59 -0500

    integracion de logs en EstudioAPI

--- Estudio.Api/Global.asax.cs
+++ Estudio.Api/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using log4net;
 
 namespace Estudio.Api
 {
@@ -13,6 +14,7 @@ namespace Estudio.Api
     {
         protected void Application_Start()
         {
+            log4net.Config.XmlConfigurator.Configure();
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
