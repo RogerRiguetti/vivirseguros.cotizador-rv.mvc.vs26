89a244eda1f5d1cf5983013b38840e3f3065fcd6
wcdzvs <william.chavez@vivirseguros.pe>
2024-12-19 16:58:32 -0500

    deleteTestJubilare

--- Estudio.Controllers/Controllers/EstudioController.cs
+++ Estudio.Controllers/Controllers/EstudioController.cs
@@ -736,41 +736,6 @@ namespace Estudio.Controllers.Controllers
             return result;
         }
 
-        #region "Prueba Jubilare"
-        private string GetNameFile()
-        {
-            string fileName = "CarteraJubilare";
-            DateTime now = DateTime.Now;
-
-            string formattedDate = now.ToString("yyyyMMdd");
-            string formattedTime = now.ToString("HHmm");
-            return $"{fileName}_{formattedDate}_{formattedTime}.xlsx";
-        }
-
-        [HttpPost]
-        public ActionResult GetCarteraCompleta()
-        {
-            _log.Info("Inicia solicitud de GetCartera Completa");
-            string folderPath = @"D:\ExportacionesJubilare";
-
-            _log.Info("Verificando la existencia del directorio ExportacionesJubilare en disco D");
-            if (!Directory.Exists(folderPath))
-            {
-                Directory.CreateDirectory(folderPath);
-                _log.Info("Se crea el directorio ExportacionesJubilare en disco D");
-            }
-
-            string fileName = GetNameFile();
-            string filePath = Path.Combine(folderPath, fileName);
-            _log.Info("Se genera el filePath");
-            JubilareExportDataPaginationLogic jubilareExportData = new JubilareExportDataPaginationLogic();
-            _log.Info("Se hace llamado del service de jubilareExportData");
-            jubilareExportData.ExportToExcelPagination(filePath);
-            _log.Info("Fin de Solicitud de GetCartera Completa");
-            return File(filePath, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
-        }
-
-        #endregion
-
+        
     }
 }
